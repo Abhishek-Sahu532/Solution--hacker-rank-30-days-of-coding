940efbfa5692b37ab1ef3876d9bf0578e2144b71
Abhishek Sahu <asahu532@gmail.com>
2022-09-22 17:33:22 +0530

    Add files via upload

---
+++ Day-0- hello world.js.js
@@ -0,0 +1,22 @@
+var string="Welcome to 30 Days of Code!";
+function processData(inputString) {
+     // Your first line of output goes here
+    process.stdout.write("Hello, World." + "\n"); 
+    
+    // Write the second line of output
+    process.stdout.write(inputString);
+}
+
+
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
